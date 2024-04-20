f77c28d849d6525fc37dfac6c9030caf06d2fe29
mariaechm <maria.chuico@unl.edu.ec>
2024-04-19 22:07:18 -0500

    Mejora de presenteacion

--- circunferencia.py
+++ circunferencia.py
@@ -1,12 +1,17 @@
 import math
 
+print("Primer commit")
+print("Bienvenidos al programa")
+print("---------------------------------------------------------------------------")
 # Coordenadas del centro de la circunferencia (h, k)
 h = float(input("Ingrese la coordenada x del centro (h): "))
 k = float(input("Ingrese la coordenada y del centro (k): "))
+print("---------------------------------------------------------------------------")
 
 # Coordenadas de un punto en la circunferencia (x, y)
 x = float(input("Ingrese la coordenada x de un punto en la circunferencia (x): "))
 y = float(input("Ingrese la coordenada y de un punto en la circunferencia (y): "))
+print("---------------------------------------------------------------------------")
 
 # Calcula el radio
 radio = math.sqrt((x - h)**2 + (y - k)**2)
@@ -18,6 +23,10 @@ ecuacion = f"(x - {h})^2 + (y - {k})^2 = {radio**2}"
 punto_medio = (h, k)
 
 # Imprime los resultados
+print("---------------------------------------------------------------------------")
 print(f"Ecuación de la circunferencia: {ecuacion}")
 print(f"Radio de la circunferencia: {radio}")
 print(f"Punto medio de la circunferencia: ({punto_medio[0]}, {punto_medio[1]})")
+print("---------------------------------------------------------------------------")
+
+print("Gracias por usar el programa")
